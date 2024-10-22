6d096844e1a141f3e523f461e9b1491de7f3c683
Arseny <kingstonaynes@gmail.com>
2024-10-23 00:02:19 +0300

    первая часть?

--- lab1.py
+++ lab1.py
@@ -3,9 +3,3 @@ def sum_numbers(numbers):
 
 numbers = [1, 5, 3, 7, 2]
 print(sum_numbers(numbers))
-
-#делаю вторую часть задания
-def max_numbers(numbers):
-    return max(numbers)
-
-print(max_numbers(numbers))
